8576071708381cb06689056ad5eb617b555925b5
Chris B. Behrens <cbehrens@gmail.com>
2022-02-23 16:10:04 -0600

    Added data types test

---
+++ CSharpPerformancePlaybook.Benchmarker/DataTypes.cs
@@ -0,0 +1,86 @@
+﻿namespace CSharpPerformancePlaybook.Benchmarker
+{
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Mathematics;
+    using BenchmarkDotNet.Order;
+
+    [ShortRunJob]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn(NumeralSystem.Arabic)]
+    public class DataTypes
+    {
+        [Benchmark]
+        public void TypeLong()
+        {
+            long x = 200;
+
+            for (var i = 0; i < x; i++)
+            {
+                var y = i;
+                y++;
+            }
+        }
+
+        [Benchmark(Baseline =true)]
+        public void TypeInt()
+        {
+            int x = 200;
+
+            for (var i = 0; i < x; i++)
+            {
+                var y = i;
+                y++;
+            }
+        }
+
+
+        [Benchmark]
+        public void TypeByte()
+        {
+            byte x = 200;
+
+            for (var i = 0; i < x; i++)
+            {
+                var y = i;
+                y++;
+            }
+        }
+
+        [Benchmark]
+        public void TypeFloat()
+        {
+            float x = 200;
+
+            for (var i = 0; i < x; i++)
+            {
+                var y = i;
+                y++;
+            }
+        }
+
+        [Benchmark]
+        public void TypeDouble()
+        {
+            double x = 200;
+
+            for (var i = 0; i < x; i++)
+            {
+                var y = i;
+                y++;
+            }
+        }
+
+
+        [Benchmark]
+        public void TypeDecimal()
+        {
+            decimal x = 200;
+
+            for (var i = 0; i < x; i++)
+            {
+                var y = i;
+                y++;
+            }
+        }
+    }
+}
--- CSharpPerformancePlaybook.Benchmarker/Program.cs
+++ CSharpPerformancePlaybook.Benchmarker/Program.cs
@@ -3,4 +3,5 @@
 using BenchmarkDotNet.Running;
 
 //var summary = BenchmarkRunner.Run(typeof(CSharpPerformancePlaybook.Benchmarker.Assignment));
-var summary = BenchmarkRunner.Run(typeof(CSharpPerformancePlaybook.Benchmarker.Perms));
+// var summary = BenchmarkRunner.Run(typeof(CSharpPerformancePlaybook.Benchmarker.Perms));
+var summary = BenchmarkRunner.Run(typeof(CSharpPerformancePlaybook.Benchmarker.DateTimeCreation));
